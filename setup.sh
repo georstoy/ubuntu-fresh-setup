88a9a6892515303f1472deb3e1a1cd6d065171da
Stoyan Georgiev <stoyan@scavasoft.com>
2020-10-31 17:45:03 +0200

    fix: Homebrew installation

--- setup.sh
+++ setup.sh
@@ -113,11 +113,11 @@ echo "Package management"
 # ###################################################################
 if [[ "${HOMEBREW}" == "YES" ]]; then
   echo "installing Homebrew (https://brew.sh/)"
-  sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
   test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
   test -r ~/.bash_profile && echo eval" ($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-  echo "eval $($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+  echo "eval $($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
 else
   echo "skipping Homebrew (https://brew.sh/) installation"
 fi
