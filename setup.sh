294a15facef9fba28ae1abdbe2c75188a2ed5425
Stoyan Georgiev <stoyan@scavasoft.com>
2020-10-31 17:03:21 +0200

    feat: update config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,56 @@
 #!/bin/bash
 
-NODEJS_VERSION=12
-SLACK_VERSION=4.4.2
-INSTALL_GIT=YES
-INSTALL_WINE=YES
-INSTALL_SKYPE=YES
+# ###################################################################
+# Virtual machines, Containers and Emulators
+DOCKER=YES        # Docker      (https://www.docker.com/)
+WINE=YES          # Wine        (https://www.winehq.org/)
+
+# ###################################################################
+# Package management
+HOMEBREW=YES      # Homebrew    (https://brew.sh/)
+NPM=YES           # npm         (https://www.npmjs.com/)
+
+# ###################################################################
+# Development environment
+
+# ### Version control systems
+GIT=YES           # Git         (https://git-scm.com/)
+SMARTGIT=YES      # SmartGit    (https://www.syntevo.com/smartgit/)
+
+# ### IDE and text editors
+SUBLIME=YES       # Sublime     (https://www.sublimetext.com/)
+WEBSTORM=YES      # WebStorm    (https://www.jetbrains.com/webstorm/)
+
+# ### Web clients
+CHROME=YES        # Chrome      (https://www.google.com/chrome/)
+POSTMAN=YES       # Postman     (https://www.postman.com/)
+AWS=YES           # AWS         (https://aws.amazon.com/cli/)
+TERRAFORM=YES     # Terraform   (https://www.terraform.io/)
 TERRAFORM_VERSION=0.12.26
 
+# ### Web servers
+OPEN_SSH=YES      # OpenSSH     (https://www.openssh.com/)
+
+# ### Runtime environments
+
+# ### ### JavaScript
+NODEJS=YES        # Node.js     (https://nodejs.org/en/)
+NODEJS_VERSION=12
+
+# ### ### JAVA
+OPEN_JDK=YES      # OpenJDK     (https://openjdk.java.net/)
+
+# ###################################################################
+# Communication tools
+SLACK=YES         # Slack       (https://slack.com/)
+SLACK_VERSION=4.4.2
+SKYPE=YES         # Skype       (https://www.skype.com/)
+
+# ###################################################################
+# Miscellaneous
+VLC=YES           # VLC player  (https://www.videolan.org/vlc/index.html)
+
+# ###################################################################
 NOTE="# Added by Ubutnu-Fresh-Setup: https://github.com/plamendp/ubuntu-fresh-setup"
 
 
@@ -44,113 +88,169 @@ sudo apt install -y \
 	build-essential file
 
 # ###################################################################
-# Wine
-if [[ "${INSTALL_WINE}" == "YES" ]]; then
-  sudo apt install -y wine64 wine32
-fi
-
-# ###################################################################
-# nodejs
-curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION}.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# ###################################################################
-# npm (cli)
-sudo apt install npm
-
-# ###################################################################
-# Homebrew (cli)
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo eval" ($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval $($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-
-# ###################################################################
-# docker (this will install docker.io as a dependancy)
-# As of Ubuntu 20.04 docker.io package is more up to date
-sudo apt install -y docker-compose
-# Add current user to docker group (require logout and login, better system reboot)
-sudo usermod -aG docker $USER
-
-# ###################################################################
-# Sublime
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-sudo apt update
-sudo apt install -y sublime-text
-
-# ###################################################################
-# WebStorm
-sudo snap install webstorm --classic
-
-# ###################################################################
-# Git (cli)
-if [[ "${INSTALL_GIT}" == "YES" ]]; then
-  sudo apt-get install git
-fi
-
-# ###################################################################
-# Smartgit
-if [[ "${INSTALL_GIT}" == "YES" ]]; then
-  curl -fsSL https://www.syntevo.com/downloads/smartgit/smartgit-20_1_1.deb > /tmp/smartgit.deb
-  sudo dpkg -i /tmp/smartgit.deb
-fi
-
-# ###################################################################
-# Postman
-sudo snap install postman
-
-# ###################################################################
-# Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-
-# ###################################################################
-# Slack
-curl -fsSL https://downloads.slack-edge.com/linux_releases/slack-desktop-${SLACK_VERSION}-amd64.deb > /tmp/slack.deb
-sudo apt install -y /tmp/slack.deb
-
-# ###################################################################
 # Gnome tweak
 sudo apt install -y gnome-tweak-tool
-
 # ###################################################################
-# Java (OpenJDK)
-sudo apt install -y default-jre
-
 # ###################################################################
-# AWS CLI v2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-mv awscliv2.zip /tmp
-unzip /tmp/awscliv2.zip -d /tmp
-sudo /tmp/aws/install
-
+echo "Virtual machines and Emulators:"
 # ###################################################################
-# Skype
-if [[ "${INSTALL_SKYPE}" == "YES" ]]; then
-  sudo snap install skype --classic
+if [[ "${DOCKER}" == "YES" ]]; then
+  echo "installing Docker (https://www.docker.com/)"
+  # this will install docker.io as a dependency
+  sudo apt install -y docker-compose
+  sudo usermod -aG docker $USER
+else
+  echo "skipping Docker (https://www.docker.com/) installation"
+fi
+if [[ "${WINE}" == "YES" ]]; then
+  echo "installing Wine (https://www.winehq.org/)"
+  sudo apt install -y wine64 wine32
+else
+  echo "skipping Wine (https://www.winehq.org/) installation"
+fi
+# ###################################################################
+echo "Package management"
+# ###################################################################
+if [[ "${HOMEBREW}" == "YES" ]]; then
+  echo "installing Homebrew (https://brew.sh/)"
+  sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+  test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  test -r ~/.bash_profile && echo eval" ($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  echo "eval $($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+else
+  echo "skipping Homebrew (https://brew.sh/) installation"
+fi
+if [[ "${NPM}" == "YES" ]]; then
+  echo "installing npm (https://www.npmjs.com/)"
+  sudo apt install npm
+else
+  echo "skipping npm (https://www.npmjs.com/) installation"
+fi
+# ###################################################################
+echo "Development environment"
+# ###################################################################
+
+echo "* Version control systems:"
+if [[ "${GIT}" == "YES" ]]; then
+  echo "installing Git (https://git-scm.com/)"
+  sudo apt-get install git
+else
+  echo "skipping Git (https://git-scm.com/) installation..."
+fi
+if [[ "${SMARTGIT}" == "YES" ]]; then
+  echo "installing SmartGit (https://www.syntevo.com/smartgit/)"
+  curl -fsSL https://www.syntevo.com/downloads/smartgit/smartgit-20_1_1.deb > /tmp/smartgit.deb
+  sudo dpkg -i /tmp/smartgit.deb
+else
+  echo "skipping SmartGit (https://www.syntevo.com/smartgit/) installation"
+fi
+
+echo "* IDE and text editors"
+if [[ "${SUBLIME}" == "YES" ]]; then
+  echo "installing Sublime (https://www.sublimetext.com/)"
+  curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
+  sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+  sudo apt update
+  sudo apt install -y sublime-text
+else
+  echo "skipping Sublime (https://www.sublimetext.com/) installation"
+fi
+if [[ "${WEBSTORM}" == "YES" ]]; then
+  echo "installing WebStorm (https://www.jetbrains.com/webstorm/)"
+  sudo snap install webstorm --classic
+else
+  echo "skipping WebStorm (https://www.jetbrains.com/webstorm/) installation"
+fi
+
+echo "* Web clients"
+if [[ "${CHROME}" == "YES" ]]; then
+  echo "installing Chrome (https://www.google.com/chrome/)"
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo apt install ./google-chrome-stable_current_amd64.deb
+else
+  echo "skipping Chrome (https://www.google.com/chrome/) installation"
+fi
+if [[ "${POSTMAN}" == "YES" ]]; then
+  echo "installing Postman (https://www.postman.com/)"
+  sudo snap install postman
+else
+  echo "skipping Postman (https://www.postman.com/) installation"
+fi
+if [[ "${AWS}" == "YES" ]]; then
+  echo "installing AWS cli (https://aws.amazon.com/cli/)"
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  mv awscliv2.zip /tmp
+  unzip /tmp/awscliv2.zip -d /tmp
+  sudo /tmp/aws/install
+else
+  echo "skipping AWS cli (https://aws.amazon.com/cli/)"
+fi
+if [[ "${TERRAFORM}" == "YES" ]]; then
+  echo "installing Terraform (https://www.terraform.io/)"
+  curl -fsSL https://releases.hashicorp.com/terraform/"${TERRAFORM_VERSION}"/terraform_"${TERRAFORM_VERSION}"_linux_amd64.zip > /tmp/terraform.zip \
+  && sudo unzip -o /tmp/terraform.zip -d /usr/local/bin \
+  && sudo chmod +x /usr/local/bin/terraform
+else
+  echo "skipping Terraform (https://www.terraform.io/)"
+fi
+
+echo " * Web servers"
+if [[ "${OPEN_SSH}" == "YES" ]]; then
+  echo "installing OpenSSH (https://openjdk.java.net/)"
+  sudo apt install -y openssh-server
+else
+  echo "skipping OpenSSH (https://openjdk.java.net/)"
 fi
 
 # ###################################################################
-# VLC
-sudo apt install -y vlc
+echo " * Runtime environments"
+# ###################################################################
+
+echo " - JavaScript"
+if [[ "${NODEJS}" == "YES" ]]; then
+  echo "installing Node.js (https://nodejs.org/en/)"
+  curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION}.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+else
+  echo "skipping Node.js (https://nodejs.org/en/) installation"
+fi
+
+echo " - Java"
+if [[ "${OPEN_JDK}" == "YES" ]]; then
+  echo "installing OpenJDK (https://openjdk.java.net/)"
+  sudo apt install -y default-jre
+else
+  echo "skipping OpenJDK (https://openjdk.java.net/)"
+fi
 
 # ###################################################################
-# SSH Server
-sudo apt install -y openssh-server
-
+echo "Communication tools"
 # ###################################################################
-# Terraform
-curl -fsSL https://releases.hashicorp.com/terraform/"${TERRAFORM_VERSION}"/terraform_"${TERRAFORM_VERSION}"_linux_amd64.zip > /tmp/terraform.zip \
-  && sudo unzip -o /tmp/terraform.zip -d /usr/local/bin \
-  && sudo chmod +x /usr/local/bin/terraform
-
+if [[ "${SLACK}" == "YES" ]]; then
+  echo "installing Slack (https://slack.com/)"
+  curl -fsSL https://downloads.slack-edge.com/linux_releases/slack-desktop-${SLACK_VERSION}-amd64.deb > /tmp/slack.deb
+  sudo apt install -y /tmp/slack.deb
+else
+  echo "skipping Slack (https://slack.com/) installation"
+fi
+if [[ "${SKYPE}" == "YES" ]]; then
+  echo "installing Skype (https://www.skype.com/)"
+  sudo snap install skype --classic
+else
+  echo "skipping Skype (https://www.skype.com/) installation"
+fi
+# ###################################################################
+echo " * Miscellaneous"
+# ###################################################################
+if [[ "${VLC}" == "YES" ]]; then
+  echo "installing VLC player (https://www.videolan.org/vlc/index.html)"
+  sudo apt install -y vlc
+else
+  echo "skipping VLC player (https://www.videolan.org/vlc/index.html)"
+fi
 
 sudo apt install -y -f
-
-
-
 # ###################################################################
 #
 # Various configuration files
